aa1c02d556f31285b406576fb318f5b4dfc7d30d
Annapurna <gantasala.annapurna@gmail.com>
2021-04-18 22:07:10 +0530

    final changes

--- TaskList/src/app/app.component.ts
+++ TaskList/src/app/app.component.ts
@@ -1,31 +1,29 @@
 import { Component} from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'TaskList';
-  check:boolean = true
-  test = ""
-  
+  title = 'TaskList';  
   taskList = [];
   onEnter(abc)
   {
     this.taskList.push(abc.value)
-    abc.value = ''
-    
+    abc.value = ''    
   }
+  
   enableButton(check)
   {
     if(check.checked)
     {
-      this.test = "button enabled";
+      (document.getElementById('edi') as HTMLInputElement).disabled = false;
+      (document.getElementById('del') as HTMLInputElement).disabled = false;
     }
     else{
-    this.test = "button disabled";
+      (document.getElementById('edi') as HTMLInputElement).disabled = true;
+      (document.getElementById('del') as HTMLInputElement).disabled = true;
     }
   }
 }
